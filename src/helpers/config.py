bceab35c811d33c4a9579fa312ba9ba6daa0c063
ahmedlapip <ahmed.a.a.labib@gmail.com>
2026-05-03 17:37:33 +0300

    add:fastAPI template

---
+++ src/helpers/config.py
@@ -0,0 +1,3 @@
+"""
+TIP: Implement the Settings class using pydantic_settings to load configuration from the .env file.
+"""
---
+++ src/main.py
@@ -0,0 +1,4 @@
+"""
+TIP: This is the main entry point of the FastAPI application. 
+Implement the FastAPI app and integrate routers and databases here according to the README.
+"""
