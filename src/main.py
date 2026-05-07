aab2c3350d463e3a8a92c3592a39e103109ac0a1
Mahmoud Attia <mahmoudattia8059@gmail.com>
2026-05-07 22:13:41 +0300

    DB-Connection-Docker-Added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from dotenv import load_dotenv
 load_dotenv('.env')
+from pathlib import Path
+from src.models.db_schemes import db_connection, close_db_connection
 from fastapi import FastAPI
 from src.routes import base ,File
-app =FastAPI()
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await db_connection()
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Server is up and running!'}
+
+@app.on_event("shutdown")
+async def shutdown():
+    await close_db_connection()
+
 app.include_router(base.base_router)
 app.include_router(File.base_router)
--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,1 +1,32 @@
-# TIP: Initialize package exports here.
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import ServerSelectionTimeoutError
+import os
+
+MONGO_URL_LOCAL = os.getenv('MONGO_URL_LOCAL')
+DB_NAME = os.getenv('MONGODB_DB_NAME')
+async def db_connection():
+    global client, db
+    try:
+        client = AsyncIOMotorClient(
+            MONGO_URL_LOCAL,
+            serverSelectionTimeoutMS=5000
+        )
+
+        # Force Connection Check
+        await client.server_info()
+
+        db = client[DB_NAME]
+        print("✅ Connected to MongoDB")
+
+    except ServerSelectionTimeoutError as e:
+        print("❌ MongoDB connection failed (timeout):")
+
+    except Exception as e:
+        print("❌ Unexpected MongoDB error:")
+
+
+async def close_db_connection():
+    global client
+    if client:
+        client.close()
+        print("🔌 MongoDB connection closed")
