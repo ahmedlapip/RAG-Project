2e6cbad8175252658b74c2951fc84bce1d7e983f
ahmedlapip <ahmed.a.a.labib@gmail.com>
2026-05-11 23:45:48 +0300

    feat:UI_v1.1

--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ from src.stores.vectordb.provider.QdrantDB_provider import QDrantProvider
 from src.stores.vectordb.vectorDBProviderFactory import VectorDBProviderFactory
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from src.routes import file, project
+from src.routes import file, project, user
 
 app = FastAPI(
     title="RAG API", description="Retrieval-Augmented Generation API", version="1.0.0"
 )
 app.include_router(project.project_router)
 app.include_router(file.base_router)
-
+app.include_router(user.router)
 
 @app.on_event("startup")
 async def start_server():
